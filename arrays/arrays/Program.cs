2b47827658a3e47e9bdaedf2f07228b45f5cda9d
NoviMar <141076996+NoviMar@users.noreply.github.com>
2024-01-17 00:17:30 +0300

    Arrays_Task
    Determine the number of array elements whose values exceed the specified value. Create a new array of these elements.

---
+++ arrays/arrays/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq.Expressions;
+
+class Class1
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите количество элементов массива:\t");
+        int SizeArray = Convert.ToInt32(Console.ReadLine());
+        decimal[] array = new decimal[SizeArray];
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write($"\n Введите элемент под индексом {i}:\t ");
+            array[i] = Convert.ToDecimal(Console.ReadLine());
+        }
+
+        Console.Write($"\nВведите крайнее число:\t");
+        int El = Convert.ToInt32(Console.ReadLine());
+
+        decimal[] FirstResult = new decimal[0];
+        foreach (int num in array)
+        {
+            if (num > El)
+            {
+                Array.Resize(ref FirstResult, FirstResult.Length + 1);
+                FirstResult[FirstResult.Length - 1] = num;
+            }
+        }
+
+        int FirstCount = 0;
+        Console.WriteLine($"\n Размер первого массива:\t{FirstResult.Length}");
+        do
+        {
+            Console.Write($"\n Элементы первого массива:\t{FirstResult[FirstCount]}");
+            FirstCount++;
+        }
+        while (FirstCount < FirstResult.Length);
+        Console.Write($"\n");
+
+        decimal[] SecondResult = Array.FindAll(array, i => i > El);
+
+        int SecondCount = 0;
+        Console.WriteLine($"\n Размер второго массива:\t{SecondResult.Length}");
+        while (SecondCount < SecondResult.Length)
+        {
+            Console.Write($"\n Элементы второго массива:\t{SecondResult[SecondCount]}");
+            SecondCount++;
+        }
+    }
+}
